4be02703b9dcc5acdbdba4b7ce7965b2872f32a4
Alexander Jampolski <aj@Alexanders-MBP.fritz.box>
2021-05-16 22:46:24 +0200

    added lesson3

--- money-manager/src/App.vue
+++ money-manager/src/App.vue
@@ -1,14 +1,89 @@
 <template>
-    <PaymentsDisplay />
+  <main class="flex-shrink-0">
+    <div class="container">
+      <h1 class="mt-5">My personal costs</h1>
+      <Button @clicked="clicked" :showFrom="showFrom" />
+      <AddPaymentForm @addNewPayment="addNewPayment" :showFrom="showFrom" />
+      <PaymentsDisplay :items="paymentsList" />
+    </div>
+  </main>
 </template>
 
 <script>
-import PaymentsDisplay from './components/PaymentsDisplay.vue'
+import PaymentsDisplay from './components/PaymentsDisplay'
+import AddPaymentForm from './components/AddPaymentForm'
+import Button from './components/Button'
 
 export default {
   name: 'App',
   components: {
-    PaymentsDisplay
+    PaymentsDisplay,
+    AddPaymentForm,
+    Button
+  },
+  data () {
+    return {
+      paymentsList: [],
+      showFrom: false
+    }
+  },
+  methods: {
+    fetchData () {
+      return [
+        {
+          id: 1,
+          date: '28.03.2020',
+          category: 'Food',
+          price: 169
+        },
+        {
+          id: 2,
+          date: '24.03.2020',
+          category: 'Transport',
+          price: 360
+        },
+        {
+          id: 3,
+          date: '23.03.2020',
+          category: 'Food',
+          price: 532
+        },
+        {
+          id: 4,
+          date: '21.03.2020',
+          category: 'Transport',
+          price: 162
+        },
+        {
+          id: 5,
+          date: '2o.03.2020',
+          category: 'Transport',
+          price: 111
+        },
+        {
+          id: 6,
+          date: '19.02.2020',
+          category: 'Food',
+          price: 162
+        },
+        {
+          id: 7,
+          date: '13.02.2020',
+          category: 'Transport',
+          price: 199
+        }
+      ]
+    },
+    addNewPayment (data) {
+      data.id = this.paymentsList[this.paymentsList.length - 1].id + 1
+      this.paymentsList = [...this.paymentsList, data]
+    },
+    clicked () {
+      this.showFrom = !this.showFrom
+    }
+  },
+  created () {
+    this.paymentsList = this.fetchData()
   }
 }
 </script>
--- money-manager/src/components/PaymentsDisplay.vue
+++ money-manager/src/components/PaymentsDisplay.vue
@@ -1,15 +1,59 @@
 <template>
-  <div class="payments-display">
-    Payments Display
+  <div>
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Date</th>
+        <th scope="col">Category</th>
+        <th scope="col">Value</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in this.pageOfItems" :key="item.id">
+        <th scope="row">{{ item.id }}</th>
+        <td>{{ item.date }}</td>
+        <td>{{ item.category }}</td>
+        <td>{{ item.price }}</td>
+      </tr>
+      </tbody>
+    </table>
+    <Pagination @currentPage="setCurrentPage" :paymentsListLength="items.length" />
   </div>
 </template>
 
 <script>
+import Pagination from './Pagination'
+
 export default {
-  name: 'PaymentsDisplay'
+  name: 'PaymentsDisplay',
+  components: {
+    Pagination
+  },
+  props: {
+    items: Array,
+    default: []
+  },
+  data () {
+    return {
+      itemsPerPage: 3,
+      currentPage: 1,
+      pageOfItems: []
+    }
+  },
+  methods: {
+    setPageOfItems () {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage
+      const endIndex = (startIndex + this.itemsPerPage) - 1
+      this.pageOfItems = this.items.slice(startIndex, endIndex + 1)
+    },
+    setCurrentPage (currentPage) {
+      this.currentPage = currentPage
+      this.setPageOfItems()
+    }
+  },
+  created () {
+    this.setPageOfItems()
+  }
 }
 </script>
-
-<style lang="scss" module>
-
-</style>
