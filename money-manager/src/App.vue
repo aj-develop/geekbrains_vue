e1465afb4eb1b5daa938ed1710c2abd659044ddc
Alexander Jampolski <aj@Alexanders-MBP.fritz.box>
2021-05-16 14:18:51 +0200

    money manager: setup

---
+++ money-manager/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <PaymentsDisplay />
+</template>
+
+<script>
+import PaymentsDisplay from './components/PaymentsDisplay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PaymentsDisplay
+  }
+}
+</script>
---
+++ money-manager/src/components/PaymentsDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="payments-display">
+    Payments Display
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentsDisplay'
+}
+</script>
+
+<style lang="scss" module>
+
+</style>
